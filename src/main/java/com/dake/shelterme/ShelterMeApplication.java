33f775a08e9ffa28b95c4fac487bc316c0be7e92
Modular-Coder <modular.coder@protonmail.com>
2018-09-30 15:16:38 -0500

    Spring intialzier skeleton

---
+++ src/main/java/com/dake/shelterme/ShelterMeApplication.java
@@ -0,0 +1,12 @@
+package com.dake.shelterme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterMeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dake/shelterme/ShelterMeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dake.shelterme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShelterMeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

cb2b33d63a93b43ed081b83b7ffa2028527027c7
Modular-Coder <modular.coder@protonmail.com>
2018-10-14 11:28:12 -0500

    auth0 config

--- src/main/java/com/dake/shelterme/ShelterMeApplication.java
+++ src/main/java/com/dake/shelterme/ShelterMeApplication.java
@@ -2,7 +2,7 @@ package com.dake.shelterme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class ShelterMeApplication {
--- src/main/java/com/dake/shelterme/controller/ShelterMeSvc.java
+++ src/main/java/com/dake/shelterme/controller/ShelterMeSvc.java
@@ -20,4 +20,24 @@ public class ShelterMeSvc {
     public List<Shelter> helloWorld() {
         return shelterRepository.findAll();
     }
+
+    @RequestMapping(value = "/api/public")
+    public String publicEndpoint() {
+        return "public endpoint called!";
+    }
+
+    @RequestMapping(value = "/api/private")
+    public String privateEndpoint() {
+        return "private endpoint called!";
+    }
+
+    @RequestMapping(value = "/api/private-scoped")
+    public String privateScopedEndpoint() {
+        return "private-scoped endpoint called!";
+    }
+
+    @RequestMapping(value = "/callback")
+    public String callBack() {
+        return "Call back success!";
+    }
 }
